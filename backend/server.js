34138fe325f28f6e6a0a29f4c83077869d394130
Teli Mushera <telimushera10@gmail.com>
2026-07-14 15:26:06 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,11 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const server = app.listen(process.env.PORT, () => {
+const PORT = process.env.PORT || 4000;
+
+const server = app.listen(PORT, () => {
   console.log(
-    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+    `Server started on PORT: ${PORT} in ${process.env.NODE_ENV} mode.`
   );
 });
 
